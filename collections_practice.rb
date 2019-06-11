299e7032807fbda8bde49f1da28d03c1b43e4505
communal-tutorial-3267 <jrodden1@gmail.com>
2019-06-11 18:00:55 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -14,12 +14,21 @@ def sort_array_char_count(array)
   end
 end
 
-def swap_elements(array, index_from, index_to)
+def swap_elements(array)
+  original_two = array[1]
+  original_three = array[2]
+
+  array[2] = original_two
+  array[1] = original_three
+
+  array
+end
+
+def swap_elements_from_to(array, index_from, index_to)
   original_from = array[index_from]
   original_to = array[index_to]
-  binding.pry
+
   array[index_to] = original_from
   array[index_from] = original_to
   array
-  binding.pry 
 end
