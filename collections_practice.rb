f1edd83f30b13d0857026d8d06fe377e91e05cc4
communal-tutorial-3267 <jrodden1@gmail.com>
2019-06-11 17:57:34 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -13,3 +13,11 @@ def sort_array_char_count(array)
     a.length <=> b.length
   end
 end
+
+def swap_elements(array, index_from, index_to)
+  original_from = array[index_from]
+  original_to = array[index_to]
+
+  array[index_to] = original_from
+  array[index_from] = original_to
+end
